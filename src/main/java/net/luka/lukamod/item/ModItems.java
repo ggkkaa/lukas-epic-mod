9b573e213f8640f781d71652a81b495df05850bc
Luka <luka.talevski.hn@gmail.com>
2024-08-23 20:10:18 +0800

    Add bananas and chocolate bananas

---
+++ src/main/java/net/luka/lukamod/item/ModItems.java
@@ -0,0 +1,34 @@
+package net.luka.lukamod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.luka.lukamod.LukasEpicMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item BANANA = registerItem("banana",
+            new Item(new Item.Settings()));
+    public static final Item BANANA_CHOCOLATE = registerItem("banana_chocolate",
+            new Item(new Item.Settings()));
+    public static final Item COMMUNISM = registerItem("communist",
+            new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(LukasEpicMod.MOD_ID, name), item);
+    }
+
+    private static void customIngredientsTools(FabricItemGroupEntries entries) {
+        entries.add(COMMUNISM);
+    }
+
+    public static void registerModItems() {
+        LukasEpicMod.LOGGER.info("Registering Mod Items for " +  LukasEpicMod.MOD_ID);
+
+        LukasEpicMod.LOGGER.info("Loading tools");
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(ModItems::customIngredientsTools);
+    }
+}
