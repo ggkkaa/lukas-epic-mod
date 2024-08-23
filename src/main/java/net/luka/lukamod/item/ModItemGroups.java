a35294e0e099f3b7edf27593eb4d327945703583
Luka <luka.talevski.hn@gmail.com>
2024-08-23 20:11:36 +0800

    Add item groups

---
+++ src/main/java/net/luka/lukamod/item/ModItemGroups.java
@@ -0,0 +1,25 @@
+package net.luka.lukamod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.luka.lukamod.LukasEpicMod;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup BANANAS = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(LukasEpicMod.MOD_ID, "itemgroup.bananas"), FabricItemGroup.builder().displayName(Text.translatable("itemgroup.bananas"))
+                    .icon(() -> new ItemStack(ModItems.BANANA)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.BANANA);
+                        entries.add(ModItems.BANANA_CHOCOLATE);
+                    }).build());
+
+    public static void registerItemGroups() {
+        LukasEpicMod.LOGGER.info("Registering Item Groups for " + LukasEpicMod.MOD_ID);
+
+    }
+}
