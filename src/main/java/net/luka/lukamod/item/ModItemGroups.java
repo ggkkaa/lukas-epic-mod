9d95becb47c0c5d8bc37b73c9270d1bcd9d7bc51
Luka <luka.talevski.hn@gmail.com>
2024-08-24 14:20:05 +0800

    Reorganize Item Group

--- src/main/java/net/luka/lukamod/item/ModItemGroups.java
+++ src/main/java/net/luka/lukamod/item/ModItemGroups.java
@@ -3,6 +3,7 @@ package net.luka.lukamod.item;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.luka.lukamod.LukasEpicMod;
+import net.luka.lukamod.block.ModBlocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
@@ -14,10 +15,13 @@ public class ModItemGroups {
     public static final ItemGroup BANANAS = Registry.register(Registries.ITEM_GROUP,
             Identifier.of(LukasEpicMod.MOD_ID, "itemgroup.lukamodgroup"), FabricItemGroup.builder().displayName(Text.translatable("itemgroup.lukamodgroup"))
                     .icon(() -> new ItemStack(ModItems.CORITE)).entries((displayContext, entries) -> {
-                        entries.add(ModItems.BANANA);
-                        entries.add(ModItems.BANANA_CHOCOLATE);
+                        entries.add(ModBlocks.CORITE_BLOCK);
+                        entries.add(ModBlocks.CORITE_ORE);
+                        entries.add(ModBlocks.DEEPSLATE_CORITE_ORE);
                         entries.add(ModItems.CORITE);
                         entries.add(ModItems.RAW_CORITE);
+                        entries.add(ModItems.BANANA);
+                        entries.add(ModItems.BANANA_CHOCOLATE);
                     }).build());
 
     public static void registerItemGroups() {
